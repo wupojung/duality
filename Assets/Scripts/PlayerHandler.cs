d6a34953b8a81a160b19314499ac5a47eb3882d9
PoJung Wu <wupojung@gmail.com>
2022-01-22 03:10:22 +0800

    追加Debug

--- Assets/Scripts/PlayerHandler.cs
+++ Assets/Scripts/PlayerHandler.cs
@@ -1,16 +1,20 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D.Path.GUIFramework;
 using UnityEngine;
 
 public class PlayerHandler : MonoBehaviour
 {
-    public bool isBusy = false; 
-    
+    public bool isBusy = false;
+
+    private BlockItem _currentBlock;
+
+    #region Unity core event
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -22,13 +26,43 @@ public class PlayerHandler : MonoBehaviour
         }
     }
 
+    private void OnGUI()
+    {
+        GUIStyle style = new GUIStyle();
+        style.fontSize = 30;
+        style.normal.textColor = Color.white;
+        int x = 0;
+        if (gameObject.name == "P2")
+        {
+            x = 150;
+        }
+
+        if (_currentBlock.currentType == BlockType.BLACK)
+        {
+            style.normal.textColor = Color.black;
+        }
+
+        GUI.Label(new Rect(x, 0, 150, 50), _currentBlock.currentType.ToString(), style);
+    }
+
+    #endregion
+
+    #region Unity Events
+
     public void OnCollisionEnter2D(Collision2D other)
     {
-      Wall wall=  other.gameObject.GetComponent<Wall>();
-      if (wall != null)
-      {
-          isBusy = false;
-      }
-      
+        Wall wall = other.gameObject.GetComponent<Wall>();
+        if (wall != null)
+        {
+            isBusy = false;
+        }
     }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        BlockItem block = other.GetComponent<BlockItem>();
+        _currentBlock = block;
+    }
+
+    #endregion
 }
