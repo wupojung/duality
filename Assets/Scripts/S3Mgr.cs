7f8965971a0644635d4381752a889573be91d50e
PoJung Wu <wupojung@gmail.com>
2022-01-21 22:31:20 +0800

    新增測試畫面

--- Assets/Scripts/S3Mgr.cs
+++ Assets/Scripts/S3Mgr.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class S3Mgr : MonoBehaviour
 {
+
+    public  GameObject objP1;
+    public GameObject objP2;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,19 @@ public class S3Mgr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //P1
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("P1 Change Color");
+        }
         
+        //P2
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("P2 Change Color");
+        }
     }
+    
+    
+    
 }
