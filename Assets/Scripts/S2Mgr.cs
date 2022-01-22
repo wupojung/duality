c969b332f3348be7cd172fccd6ce0972d9c1f0d1
RayChen1996 <53119155+RayChen1996@users.noreply.github.com>
2022-01-22 17:52:32 +0800

    S2 測試二

--- Assets/Scripts/S2Mgr.cs
+++ Assets/Scripts/S2Mgr.cs
@@ -6,6 +6,7 @@ using UnityEngine.Events;
 
 public class S2Mgr : MonoBehaviour
 {
+    public GameObject PressKeyText;
     public GameObject w_key, s_key, d_Key, a_key;
     public GameObject up_key, down_key, right_Key, left_key;
     public Sprite w_keySprite_down, s_keySprite_down, d_KeySprite_down, a_keySprite_down;
@@ -29,8 +30,37 @@ public class S2Mgr : MonoBehaviour
         spriteRenderer_d = d_Key.GetComponent<SpriteRenderer>();
 
 
+        //StartCoroutine(ExampleCoroutine());
+        StartCoroutine(ChangeColor());
     }
-  
+    IEnumerator ExampleCoroutine()
+    {
+        ChgTextStatus();
+        yield return new WaitForSeconds(2);
+
+    }
+    void TextControlVisible()
+    {
+        PressKeyText.active = !PressKeyText.active;
+       
+
+    }
+    public float delaySeconds = .5f;
+
+    IEnumerator ChangeColor()
+    {
+        //WaitUntil waitForFlag = new WaitUntil(() => doChangeColor);
+
+        while (true)
+        {
+            //yield return waitForFlag;
+
+            PressKeyText.active = !PressKeyText.active;
+
+            yield return new WaitForSeconds(delaySeconds);
+        }
+    }
+
     void Chg_w()
     {
       w_key.gameObject.GetComponent<Image>().sprite = w_keySprite_keyUp;
@@ -74,30 +104,37 @@ public class S2Mgr : MonoBehaviour
         right_Key.gameObject.GetComponent<Image>().sprite = right_KeySprite_keyUp;
 
     }
+
+    void ChgTextStatus()
+    {
+        Invoke("TextControlVisible", 2F);
+    }
     // Update is called once per frame
     void Update()
     {
+
+
         if (Input.GetKeyDown(KeyCode.W))
         {
             w_key.gameObject.GetComponent<Image>().sprite = w_keySprite_down;
             
-            Invoke("Chg_w", 1);
+            Invoke("Chg_w", .5F);
        
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
             a_key.gameObject.GetComponent<Image>().sprite = a_keySprite_down;
-            Invoke("Chg_a", 1);
+            Invoke("Chg_a", .5F);
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
             s_key.gameObject.GetComponent<Image>().sprite = s_keySprite_down;
-            Invoke("Chg_s", 1);
+            Invoke("Chg_s", .5F);
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
             d_Key.gameObject.GetComponent<Image>().sprite = d_KeySprite_down;
-            Invoke("Chg_d", 1);
+            Invoke("Chg_d", .5F);
         }
 
 
@@ -105,24 +142,26 @@ public class S2Mgr : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             up_key.gameObject.GetComponent<Image>().sprite = up_keySprite_down;
-            Invoke("Chg_up", 1);
+            Invoke("Chg_up", .5F);
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             down_key.gameObject.GetComponent<Image>().sprite = down_keySprite_down;
-            Invoke("Chg_down", 1);
+            Invoke("Chg_down", .5F);
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             left_key.gameObject.GetComponent<Image>().sprite = left_keySprite_down;
-            Invoke("Chg_left", 1);
+            Invoke("Chg_left", .5F);
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             right_Key.gameObject.GetComponent<Image>().sprite = right_KeySprite_down;
-            Invoke("Chg_right", 1);
+            Invoke("Chg_right", .5F);
         }
 
+        
+        //TextControlVisible();
 
     }
 }
